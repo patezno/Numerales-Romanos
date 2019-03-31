b7e0b3c5eb8da109f2445c1c2256b9774ddd7c72
unknown <patezno97@gmail.com>
2019-03-31 19:31:13 +0200

    starting

---
+++ src/main/java/RomanPattern.java
@@ -0,0 +1,13 @@
+public class RomanPattern {
+
+    // Atributos
+
+    private String grupoSumatorio = "[MCXI]{3}|(?<!C)[DM]|(?<!X)[LC](?!D)|(?<!I)[VX](?![LC])|I(?![VX])";
+    private String grupoSustractivo = "(C[DM])|(X[LC])|(I[VX])";
+
+    public void verificarString() {}
+
+    public int encontrarLetra(String fuente) {
+    }
+
+}
